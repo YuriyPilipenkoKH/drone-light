8b5e7f2243985f9f0a68568c53ae4bb0be6e6269
YuriyPilipenkoKH <113132127+YuriyPilipenkoKH@users.noreply.github.com>
2023-01-03 16:37:43 +0000

    Deploying to gh-pages from @ YuriyPilipenkoKH/drone-light@4a07f3f41516878ad90dc103b63af5ab015bdb05 🚀

---
+++ order_page.0c38b89a.js
@@ -0,0 +1,2 @@
+document.body.onclick=function(t){if((t=t||window.event).target.classList.contains("goods-min")){for(var e=document.querySelectorAll("goods-img-min div"),o=0;o<e.length;o+=1)e[o].classList.remove("active");document.getElementById("goods-max").src=t.target.src,t.target.parentElement.classList.add("active")}};
+//# sourceMappingURL=order_page.0c38b89a.js.map
