40344c131c32fc947b9a8c3417e5d2ce5ad3f699
YuriyPilipenkoKH <113132127+YuriyPilipenkoKH@users.noreply.github.com>
2023-01-06 19:14:24 +0000

    Deploying to gh-pages from @ YuriyPilipenkoKH/drone-light@97d64b1e4ef5b5ce261a5879d6651e9323a52819 🚀

---
+++ order_page.88f6d565.js
@@ -0,0 +1,2 @@
+var burgerMenu=document.getElementById("burger-menu"),overlay=document.getElementById("menu");burgerMenu.addEventListener("click",(function(){this.classList.toggle("close"),overlay.classList.toggle("overlay")}));
+//# sourceMappingURL=order_page.88f6d565.js.map
